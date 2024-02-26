5285426d352fbc04b3e57b79985c74a9542e51c2
IvanR96 <irios96@hotmail.com>
2024-02-25 23:18:56 -0800

    new start

--- src/index.js
+++ src/index.js
@@ -2,10 +2,3 @@ import _ from 'lodash';
 import './style.css';
 
 
-const div = document.getElementById('content');
-
-const para = document.createElement('p');
-
-para.textContent = "This is a js Test!";
-
-div.appendChild(para);
