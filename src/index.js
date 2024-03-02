5b7df80ada40a5bc2f701be968f39b72727b5c9c
IvanR96 <irios96@hotmail.com>
2024-03-01 23:26:09 -0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
 import './style.css';
-import 'menu-burger.svg';
 
 
+var dropdownBtn = document.querySelector('.dropbtn');
 
+  // Get the dropdown content element
+  var dropdownContent = document.querySelector('.dropdown-content');
 
-
+  // Add event listener to the dropdown button
+  dropdownBtn.addEventListener('click', function() {
+    // Toggle the "open" class on the dropdown content
+    dropdownContent.classList.toggle('open');
+  });
