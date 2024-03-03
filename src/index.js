eb515a0d9d357342b689f8c3d7fa92e5ab050807
IvanR96 <irios96@hotmail.com>
2024-03-02 23:04:22 -0800

    removed lines

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './style.css';
 
 var dropdownBtn = document.querySelector('.dropbtn');
 
-  // Get the dropdown content element
+  
   var dropdownContent = document.querySelector('.dropdown-content');
 
-  // Add event listener to the dropdown button
+  
   dropdownBtn.addEventListener('click', function() {
-    // Toggle the "open" class on the dropdown content
+    
     dropdownContent.classList.toggle('open');
   });
